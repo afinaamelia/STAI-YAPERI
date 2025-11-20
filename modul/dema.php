d809c9cc4fade3de5ea2a62c6e29982445c27268
wawasukakiko09-web <wawasukakiko09@gmail.com>
2025-11-20 20:43:31 +0700

    wawa

--- modul/dema.php
+++ modul/dema.php
@@ -3,7 +3,7 @@
     <!-- Page Header Start -->
 <style>
     .page-header {
-    background: linear-gradient(rgba(0, 0, 0, .75), rgba(0, 0, 0, .75)), url(../img/ft4.jpg) center center no-repeat;
+    background: linear-gradient(rgba(0, 0, 0, .75), rgba(0, 0, 0, .75)), url(img/ft4.jpg) center center no-repeat;
     background-size: cover;
 }
 /* Responsif untuk HP, tablet, iPad */
@@ -28,7 +28,7 @@
     </div>
     <!-- Page Header End -->
     <!-- Work Process -->
-    <div id="about" >
+    <div id="dema" >
   <div class="container-xxl py-6">
     <div class="container">
       <!-- Tambahkan class flex-row-reverse agar gambar di kanan -->
@@ -134,187 +134,4 @@
       position: relative;
       top: -50px;
     }
-/* Default desktop */
-.logo-footer {
-  width: 26.25rem;
-  height: auto;
-  margin-top: -2.7rem;
-}
-
-.footer-wrapper {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.footer-right {
-  display: flex;
-  justify-content: flex-end;
-  padding-right: 1.25rem;
-}
- .social-f {
-  width: 2.25rem;           /* sebelumnya 36px */
-  height: 2rem;             /* sebelumnya 32px */
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 25%;
-  background: #ffc107;
-  color: #000;  
-  text-decoration: none;
-  font-size: 0.875rem;      /* sebelumnya 14px */
-  margin-left: 0.625rem;    /* sebelumnya 10px */
-  transition: 0.3s;
-}
-
-.social-f:hover {
-  background: #fff;
-  color: #000;
-}
-/* Bagian kontak footer */
-.col-lg-3.col-md-6.mb-4.mb-md-0 {
-  text-align: left; /* Pastikan semua isi rata kiri */
-  }
-
-.kontak h5 {
-  margin-left: 1.5rem; /* Pastikan judul kontak mepet kiri */
-  margin-top:1rem;
-}
-.jam h5 {
-  margin-left: 1.5rem; /* Pastikan judul kontak mepet kiri */
-  margin-top:1rem;
-}
-
-
-/* List kontak */
-.fa-ul {
-  margin-left: 0; /* Hilangkan indent bawaan */
-  padding-left: 0;
-  list-style: none;
-}
-
-.fa-ul li {
-  display: flex;
-  align-items: center; /* Sejajarkan icon & teks secara vertikal */
-  gap: 8px; /* Jarak kecil antara icon dan teks */
-  text-align: left;
-}
-
-.fa-ul .fa-li {
-  position: static; /* Hilangkan posisi absolut bawaan Font Awesome */
-  width: auto;
-  margin-right: 6px; /* Kasih jarak kecil biar icon gak nempel */
-}
-
-.fa-ul li span.ms-2 {
-  margin-left: 0 !important; /* Hapus margin ekstra dari Bootstrap */
-}
-/* Mobile & Tablet */
-@media (max-width: 992px) {
-    .col-lg-3.col-md-6.mb-4.mb-md-0 {
-  text-align: left; /* Pastikan semua isi rata kiri */
-  margin-top:-2rem;
-  }
-  .kontak h5 {
-  margin-left: 1.5rem; /* Pastikan judul kontak mepet kiri */
-  margin-top:1rem;
-}
-.jam h5 {
-  margin-left: 1.5rem; /* Pastikan judul kontak mepet kiri */
-  margin-top:3rem;
-}
-
-  footer {
-    text-align: left;
-    width: 100%;
-      }
-
-  .footer-wrapper {
-    flex-direction: column;
-    align-items: flex-start; /* semua item ke kiri */
-    justify-content: flex-start;
-    text-align: left;
-    gap: 0.75rem;
-    width: 100%;
-  }
-
-  .footer-left,
-  .footer-center,
-  .footer-right {
-    text-align: left;
-    width: 100%;
-  }
-
-  .footer-right {
-    justify-content: flex-start;
-    margin-top: 0.625rem;
-  }
-
-  .footer-bottom {
-    text-align: left;
-    font-size: 0.8rem;
-    margin-top: 0.5rem;
-    width: 100%;
-  }
-
-  footer img.logo-footer {
-    max-width: 100%;
-    height: auto;
-    margin: 0 0 0.5rem 0; /* nempel kiri */
-    display: block;
-  }
-}
-
-@media (max-width: 768px) {
-  /* Semua kolom rata kiri */
-  footer .col-lg-4,
-  footer .col-lg-3,
-  footer .col-md-6,
-  footer .col-md-12 {
-    text-align: left !important;
-    margin-left: 1.2em !important;
-    padding-left: 0 !important;
-  }
-
-  /* Logo mengecil dan ke kiri */
-  footer img.logo-footer {
-    max-width: 86%;
-    height: auto;
-    margin-left: 0;
-    margin-top: 0;
-  }
-footer table {
-    width: 100%;             /* table mengikuti lebar container */
-    margin-left: 0;          /* rapat kiri */
-    table-layout: auto;      /* kolom menyesuaikan isi */
-    border-collapse: collapse; 
-  }
-
-  footer table td, footer table th {
-    text-align: left;        /* rata kiri */
-    padding: 0.25rem 0.5rem; /* kecilkan padding supaya muat */
-    font-size: 0.85rem;      /* teks sedikit lebih kecil */
-    white-space: normal;     /* biar teks bisa wrap */
-  }
-  /* List info rata kiri */
-  footer ul.fa-ul {
-    margin-left: 2em !important;
-  }
-  .footer-left {
-    font-size: 0.60rem;   /* lebih kecil dari desktop */
-  }
-}
 </style>
-
-    <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../lib/wow/wow.min.js"></script>
-    <script src="../lib/easing/easing.min.js"></script>
-    <script src="../lib/waypoints/waypoints.min.js"></script>
-    <script src="../lib/owlcarousel/owl.carousel.min.js"></script>
-
-    <!-- Template Javascript -->
-    <script src="../js/main.js"></script>
-</body>
-</html>
